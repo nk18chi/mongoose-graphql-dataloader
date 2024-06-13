93185ef42d688830dc752da5fade5b6b44c2fa0b
Naoki Mita <naoki.mita18@gmail.com>
2024-06-12 22:47:33 -0700

    feat(SeedDB): seed initial user data into db

--- src/mongo/connect.ts
+++ src/mongo/connect.ts
@@ -1,8 +1,10 @@
 import { connect } from 'mongoose';
 import logger from '../config/logger';
+import seedData from './seed';
 
 const connectMongoDB = async () => {
   await connect(process.env.MONGO_URI);
+  await seedData();
 
   // eslint-disable-next-line no-console
   logger.info(`🚀 MongoDB ready with ${process.env.MONGO_URI}`);
