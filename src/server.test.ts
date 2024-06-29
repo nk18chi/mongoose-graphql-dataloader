62ed6d16cb504be962fe97f4261534e87e964307
Naoki Mita <naoki.mita18@gmail.com>
2024-06-28 22:31:51 -0700

    test(App): add unit test for checking dataloader in apollo server context
    since
    dataloader
    is
    in
    apllo
    server
    context

--- src/server.test.ts
+++ src/server.test.ts
@@ -1,8 +1,10 @@
-import { expect, test, describe, vi, beforeEach } from 'vitest';
+import { expect, test, describe, vi, beforeEach, Mock } from 'vitest';
 import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
 import connectMongoDB from './mongo/connect';
 import runServer from './server';
 import logger from './config/logger';
+import userDataLoader from './dataloader/User.dataLoader';
 
 vi.mock('./mongo/connect');
 vi.mock('express', () => ({
@@ -35,6 +37,14 @@ describe('server.ts', () => {
   test('start apollo server', async () => {
     await runServer();
     expect(ApolloServer.prototype.start).toHaveBeenCalledTimes(1);
+  });
+  test('start apollo server', async () => {
+    await runServer();
+    expect(await (expressMiddleware as Mock).mock.calls[0][1].context()).toEqual({
+      dataLoaders: {
+        userDataLoader,
+      },
+    });
   });
   test('listen localhost:4000', async () => {
     await runServer();
