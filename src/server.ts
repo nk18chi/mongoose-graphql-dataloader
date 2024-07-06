55f491247f57e36450f5db66a71c3510935aa500
Naoki Mita <naoki.mita18@gmail.com>
2024-07-06 12:07:32 -0700

    fix(env): add a new break line at the end to insert a new line in ci

--- src/server.ts
+++ src/server.ts
@@ -19,7 +19,7 @@ import Context from './graphql/interface/Context.interface';
 import permissions from './graphql/authorizations/permissions';
 
 const runServer = async () => {
-  console.log('MONGO_URI', process.env.MONGO_URI);
+  console.log('MONGO_URI', process.env.MONGO_URI === 'mongodb://127.0.0.1:27017/mongoose-graphql-dataloader');
   console.log('a');
   const app = express();
   const httpServer = http.createServer(app);
