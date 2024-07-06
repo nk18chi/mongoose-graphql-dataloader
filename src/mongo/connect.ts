15c4e62b7903d4c61e4d88afa4e408e6bfb8374e
Naoki Mita <naoki.mita18@gmail.com>
2024-07-06 12:12:02 -0700

    chore(log): set console log in connectMongoDB

--- src/mongo/connect.ts
+++ src/mongo/connect.ts
@@ -3,9 +3,13 @@ import logger from '../config/logger';
 import seedData from './User.seed';
 
 const connectMongoDB = async () => {
+  console.log('!');
   await connect(process.env.MONGO_URI);
+  console.log('!!');
   set('debug', true);
+  console.log('!!!');
   await seedData();
+  console.log('!!!!');
   logger.info(`🚀 MongoDB ready with ${process.env.MONGO_URI}`);
 };
 
