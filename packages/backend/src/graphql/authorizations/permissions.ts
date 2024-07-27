d34ab8735148e89ff58f6b4d8689821e1fa99091
Naoki Mita <naoki.mita18@gmail.com>
2024-07-26 21:37:15 -0700

    feat(Graphql): show external error message in graphql shield
    show the detailed error message for debug and log

--- packages/backend/src/graphql/authorizations/permissions.ts
+++ packages/backend/src/graphql/authorizations/permissions.ts
@@ -1,13 +1,18 @@
 import { shield, allow } from 'graphql-shield';
 import isAuthenticated from './rules/isAuthenticated.rule';
 
-const permissions = shield({
-  Query: {
-    '*': isAuthenticated,
-    getUsers: allow,
-    optimizedGetUsers: allow,
-    userToken: allow,
+const permissions = shield(
+  {
+    Query: {
+      '*': isAuthenticated,
+      getUsers: allow,
+      optimizedGetUsers: allow,
+      userToken: allow,
+    },
   },
-});
+  {
+    allowExternalErrors: true,
+  },
+);
 
 export default permissions;
